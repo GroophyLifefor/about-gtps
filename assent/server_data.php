093ad132f2259e3fc8d988a29b1eb50fac8efb67
Groophy-Lifefor <77299279+Groophy-Lifefor@users.noreply.github.com>
2021-03-04 14:48:59 +0300

    Add files via upload

---
+++ assent/server_data.php
@@ -0,0 +1,11 @@
+server|13.67.35.31
+port|17091
+type|1
+#maint|Mainetrance message (Not used for now) -- Growtopia Noobs
+
+beta_server|127.0.0.1
+beta_port|17091
+
+beta_type|1
+meta|localhost
+RTENDMARKERBS1001
